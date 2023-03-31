588849a618b9d953a320cc76f8b4805fb6805380
ltaralli <lisandrotaralli@gmail.com>
2023-03-31 16:48:27 -0300

    solucionando filtrado - Filtro por params ok

--- src/components/ItemListContainer/index.jsx
+++ src/components/ItemListContainer/index.jsx
@@ -1,50 +1,34 @@
-import React from 'react'
 import styles from './itemListContainer.modules.scss'
 import { useEffect, useState} from "react";
 import CardProduct from '../CardProduct';
-import { NavLink } from 'react-router-dom';
+import { useParams } from "react-router-dom";
 
-const ItemListContainer = () => {
+function ItemListContainer() {
+  const { categoryid } = useParams();
+  const [products, setProducts] = useState([]);
 
-  const [productos, setProductos] = useState ([])
-  const [categorias, setCategorias] = useState([]);
-  const [categoriaSeleccionada, setCategoriaSeleccionada] = useState("");
+  console.log(useParams())
 
   useEffect(() => {
-    fetch("https://fakestoreapi.com/products")
-      .then((response) => response.json())
-      .then((data) => {
-        setProductos(data);
-        const categoriasUnicas = [
-          ...new Set(data.map((producto) => producto.category)),
-        ];
-        setCategorias(categoriasUnicas);
-      });
-  }, []);
+    const fetchProducts = async () => {
+      let url = "https://fakestoreapi.com/products";
+      if (categoryid) {
+        url += `/category/${categoryid}`;
+      }
+      const response = await fetch(url);
+      const data = await response.json();
+      setProducts(data);
+    };
+    fetchProducts();
+  }, [categoryid]);
 
-  const handleCategoriaClick = (categoria) => {
-    setCategoriaSeleccionada(categoria);
-  };
 
-  const productosFiltrados = categoriaSeleccionada
-    ? productos.filter(
-        (producto) => producto.category === categoriaSeleccionada
-      )
-    : productos;
 
-    
   return (
     <section className='product'>
       <h2 className='product__title'>Nuestros productos</h2>
-      <div className='product__list'>
-        <h4>CATEGORIAS</h4>
-        <NavLink className={'product__navlink'} to={"/"} onClick={() => setCategoriaSeleccionada("")}> Todas las categorías</NavLink>
-            {categorias.map((categoria) => (
-              <NavLink className={'product__navlink'} key={categoria} to={`/category/${categoria}`} onClick={() => handleCategoriaClick(categoria)}> {categoria} </NavLink>
-          ))}
-      </div>
         <div className='product__container'>
-        {productosFiltrados.map((producto) => (
+        {products.map((producto) => (
         <CardProduct key={producto.id} producto={producto} />
       ))}
       </div>
--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -2,36 +2,47 @@ import React from 'react'
 import styles from './navbar.modules.scss'
 import CartWidget from '../CartWidget';
 import { NavLink } from 'react-router-dom';
+import { useEffect, useState } from "react";
 
 const Navbar = () => {
-  
-  const activeStyle = {
-    color: "rgb(252, 140, 158)"
-  }
-  
-  const notActiveStyle = {
-    color: "black",
-  }
+  const [categories, setCategories] = useState([]);
+
+  useEffect(() => {
+    const fetchCategories = async () => {
+      const response = await fetch("https://fakestoreapi.com/products/categories");
+      const data = await response.json();
+      setCategories(data);
+    };
+    fetchCategories();
+  }, []);
 
   return (
     
     <header className='navbar'>
-    <div className='navbar__nav'>
-      <div>
-      <a className='navbar__BtnImg' href="/"><img className='navbar__logo' src="../media/navbar/logo.png" alt="Logo" /></a>
-      </div>
-     <div className='navbar__toggles'>
-        <ul className='navbar__list'>
-          <NavLink to="/" style={({isActive}) => (isActive ? activeStyle : notActiveStyle)} ><p className='navbar__anchor'>Productos</p> </NavLink>
-          <NavLink to="/QuienesSomos" style={({isActive}) => (isActive ? activeStyle : notActiveStyle)} ><p className='navbar__anchor'>Quienes Somos</p> </NavLink>
-          <NavLink to="/FAQ" style={({isActive}) => (isActive ? activeStyle : notActiveStyle)} ><p className='navbar__anchor'>FAQ</p> </NavLink>
-          <NavLink to="/Contacto" style={({isActive}) => (isActive ? activeStyle : notActiveStyle)} ><p className='navbar__anchor'>Contacto</p> </NavLink>
-        </ul>
+      <div className='navbar__nav'>
         <div>
-          <CartWidget />
+          <a className='navbar__BtnImg' href="/"><img className='navbar__logo' src="../media/navbar/logo.png" alt="Logo" /></a>
+        </div>
+       <div className='navbar__toggles'>
+          <ul className='navbar__list'>
+            <li>
+              {categories.map((category) => (
+                <NavLink key={category} to={`/category/${category}`} className="active navbar__anchor">{category}</NavLink>
+              ))}
+            </li>
+            <li>
+                <NavLink to="/" className="active">All Products</NavLink>
+            </li>  
+              {/* <NavLink to="/" style={({isActive}) => (isActive ? activeStyle : notActiveStyle)} ><p className='navbar__anchor'>Productos</p> </NavLink>
+              <NavLink to="/QuienesSomos" style={({isActive}) => (isActive ? activeStyle : notActiveStyle)} ><p className='navbar__anchor'>Quienes Somos</p> </NavLink>
+              <NavLink to="/FAQ" style={({isActive}) => (isActive ? activeStyle : notActiveStyle)} ><p className='navbar__anchor'>FAQ</p> </NavLink>
+              <NavLink to="/Contacto" style={({isActive}) => (isActive ? activeStyle : notActiveStyle)} ><p className='navbar__anchor'>Contacto</p> </NavLink> */}
+          </ul>
+          <div>
+              <CartWidget />
+          </div>
         </div>
       </div>
-    </div>
     </header>
   )
 };
